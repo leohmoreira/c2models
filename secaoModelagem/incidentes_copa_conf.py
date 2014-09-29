59d6c16d990eff38e0ed79ef0ee9aa102cf3394f
Moreira <moreira@cds.eb.mil.br>
2014-09-29 20:25:24 -0300

    Plot_Date com dateformatter

--- secaoModelagem/incidentes_copa_conf.py
+++ secaoModelagem/incidentes_copa_conf.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*- 	
-from datetime import datetime
+import datetime
 import collections
 import argparse
 import numpy as np
 from scipy import special, optimize
 import matplotlib.pyplot as plt
-import scipy.stats as sta
 import time
+from matplotlib.dates import DateFormatter
 
 
 def read_file(fileName):
@@ -16,13 +16,18 @@ def read_file(fileName):
     return tuple(open(fileName, 'r'))
     
 def dateConverter(data):
+	
 	return data[:data.index(":")-1]
 
 def qtdIncidents(data):
+	
 	return data[data.index(":")+2:-1]
 
 if __name__ == "__main__":
     
+    inicioCopaConf = datetime.datetime(2013,6,15)
+    terminoCopaConf = datetime.datetime(2013,6,30)
+
     datesFromFile = read_file('clusterizado.c2')
     dates = map(dateConverter, datesFromFile)
     incidentes = map(qtdIncidents, datesFromFile)
@@ -35,13 +40,22 @@ if __name__ == "__main__":
     axisY = []
     
     for key in sortedDatesIncidents:
-    	axisX.append(datetime.strptime(key,"%Y/%m/%d"))
-    	axisY.append(datesIncidents[key])
-    
-    plt.plot_date(axisX,axisY,fmt='-',xdate=True,ydate=False)
-    plt.xticks(axisX, rotation=45)
+    	tmpDate = datetime.datetime.strptime(key,"%Y/%m/%d")
+    	if(inicioCopaConf <= tmpDate and tmpDate<=terminoCopaConf):
+    		  		
+    	    axisX.append(datetime.datetime.strptime(key,"%Y/%m/%d"))
+            axisY.append(datesIncidents[key])
+
+    	    
+    fig, ax = plt.subplots()
+    ax.set_title("Incidentes na Copa das Confederacoes - Jun 13")
+    ax.set_ylabel("Quantidade")
+    ax.set_xlabel("Dias")
+    ax.plot_date(axisX,axisY,fmt='-ro',xdate=True,ydate=False)
+    plt.xticks(axisX,rotation=45)
+    ax.xaxis.set_major_formatter(DateFormatter("%d/%B"))
     plt.grid(True)
-    plt.show()
+    #plt.show()
     plt.savefig("plot.png",dpi=96)
     
     
