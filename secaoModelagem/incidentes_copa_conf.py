a47408ee3aae4c50e6591904e5bf73a01cf0feab
Moreira <moreira@cds.eb.mil.br>
2014-10-03 12:01:31 -0300

    mais uma passo

--- secaoModelagem/incidentes_copa_conf.py
+++ secaoModelagem/incidentes_copa_conf.py
@@ -53,13 +53,13 @@ def plot_graph_actions(axisX, axisY):
     plt.show()
     #plt.savefig("plot.png",dpi=96)
 
-def plot_graph(axisX, axisY):
+def plot_graph(axisX, axisY,actions):
 
     fig, graph1 = plt.subplots()
     graph1.set_title("Incidentes na Copa das Confederacoes - Jun 13")
     graph1.set_ylabel("Quantidade")
     graph1.set_xlabel("Dias")
-    graph1.plot(axisX,axisY, 'bo')
+    graph1.plot(axisX,axisY, 'bo',axisX,actions, 'ro-')
     graph1.xaxis.set_major_formatter(DateFormatter("%d/%m"))
     plt.xticks(axisX,rotation=90)
     graph1.grid(True)
@@ -67,15 +67,15 @@ def plot_graph(axisX, axisY):
     #plt.savefig("plot.png",dpi=96)
 
 def plot_total(graphType,
-                axisXtotal, axisYtotal,
-                axisX1, axisY1,
-                axisX2, axisY2,
-                axisX3, axisY3,
-                axisX4, axisY4,
-                axisX5, axisY5,
-                axisX6, axisY6,
-                axisX7, axisY7,
-                axisX8, axisY8,
+                axisXtotal, axisYtotal,actionsTotal,
+                axisX1, axisY1,actions1,
+                axisX2, axisY2,actions2,
+                axisX3, axisY3,actions3,
+                axisX4, axisY4,actions4,
+                axisX5, axisY5,actions5,
+                axisX6, axisY6,actions6,
+                axisX7, axisY7,actions7,
+                axisX8, axisY8,actions8
             ):
 
     plt.close('all')
@@ -95,7 +95,7 @@ def plot_total(graphType,
     graphTotal.set_ylabel("Quantidade")
     graphTotal.set_xlabel("Dias")
     if (graphType == 'line'):
-        graphTotal.plot(axisX,axisY, 'bo-')
+        graphTotal.plot(axisXtotal,axisYtotal, 'bo-',axisX,actionsTotal, 'ro-')
     elif(graphType == 'bar'):
         graphTotal.bar(axisX,axisY, align='center')
         graphTotal.xaxis_date()
@@ -108,7 +108,7 @@ def plot_total(graphType,
     graphCOC.set_ylabel("Quantidade")
     graphCOC.set_xlabel("Dias")
     if (graphType == 'line'):
-        graphCOC.plot(axisX1,axisY1, 'bo-')
+        graphCOC.plot(axisX1,axisY1, 'bo-',axisX1,actions1, 'ro-')
     elif(graphType == 'bar'):
         graphCOC.bar(axisX1,axisY1, align='center')
         graphCOC.xaxis_date()
@@ -121,7 +121,7 @@ def plot_total(graphType,
     graphCC2FTCSSA.set_ylabel("Quantidade")
     graphCC2FTCSSA.set_xlabel("Dias")
     if (graphType == 'line'):
-        graphCC2FTCSSA.plot(axisX2,axisY2, 'bo-')
+        graphCC2FTCSSA.plot(axisX2,axisY2, 'bo-',axisX2,actions2, 'ro-')
     elif(graphType == 'bar'):
         graphCC2FTCSSA.bar(axisX2,axisY2, align='center')
         graphCC2FTCSSA.xaxis_date()
@@ -133,7 +133,7 @@ def plot_total(graphType,
     graphFOR.set_ylabel("Quantidade")
     graphFOR.set_xlabel("Dias")
     if (graphType == 'line'):
-        graphFOR.plot(axisX3,axisY3, 'bo-')
+        graphFOR.plot(axisX3,axisY3, 'bo-',axisX3,actions3, 'ro-')
     elif(graphType == 'bar'):
         graphFOR.bar(axisX3,axisY3, align='center')
         graphFOR.xaxis_date()
@@ -145,7 +145,7 @@ def plot_total(graphType,
     graphREC.set_ylabel("Quantidade")
     graphREC.set_xlabel("Dias")
     if (graphType == 'line'):
-        graphREC.plot(axisX4,axisY4, 'bo-')
+        graphREC.plot(axisX4,axisY4, 'bo-',axisX4,actions4, 'ro-')
     elif(graphType == 'bar'):
         graphREC.bar(axisX4,axisY4, align='center')
         graphREC.xaxis_date()
@@ -157,7 +157,7 @@ def plot_total(graphType,
     graphSSA.set_ylabel("Quantidade")
     graphSSA.set_xlabel("Dias")
     if (graphType == 'line'):
-        graphSSA.plot(axisX5,axisY5, 'bo-')
+        graphSSA.plot(axisX5,axisY5, 'bo-',axisX5,actions5, 'ro-')
     elif(graphType == 'bar'):
         graphSSA.bar(axisX5,axisY5, align='center')
         graphSSA.xaxis_date()
@@ -169,7 +169,7 @@ def plot_total(graphType,
     graphRIO.set_ylabel("Quantidade")
     graphRIO.set_xlabel("Dias")
     if (graphType == 'line'):
-        graphRIO.plot(axisX6,axisY6, 'bo-')
+        graphRIO.plot(axisX6,axisY6, 'bo-',axisX6,actions6, 'ro-')
     elif(graphType == 'bar'):
         graphRIO.bar(axisX6,axisY6, align='center')
         graphRIO.xaxis_date()
@@ -181,7 +181,7 @@ def plot_total(graphType,
     graphBSB.set_ylabel("Quantidade")
     graphBSB.set_xlabel("Dias")
     if (graphType == 'line'):
-        graphBSB.plot(axisX7,axisY7, 'bo-')
+        graphBSB.plot(axisX7,axisY7, 'bo-',axisX7,actions7, 'ro-')
     elif(graphType == 'bar'):
         graphBSB.bar(axisX7,axisY7, align='center')
         graphBSB.xaxis_date()
@@ -194,7 +194,7 @@ def plot_total(graphType,
     graphBHZ.set_ylabel("Quantidade")
     graphBHZ.set_xlabel("Dias")
     if (graphType == 'line'):
-        graphBHZ.plot(axisX8,axisY8, 'bo-')
+        graphBHZ.plot(axisX8,axisY8, 'bo-',axisX8,actions8, 'ro-')
     elif(graphType == 'bar'):
         graphBHZ.bar(axisX8,axisY8, align='center')
         graphBHZ.xaxis_date()
@@ -255,6 +255,18 @@ def get_incidents_by_cop_from_DB(inicioCopaConf,terminoCopaConf,cop):
     
     return incidentsDates
 
+def get_incidents_by_cop_per_date_from_DB(date,cop):
+
+    clusteringDefault = '%Y/%m/%d'
+    #clusterPorHora = '%Y/%B/%d %H:%m:%S'
+    incidents = Incident.get_all()
+    incidentsDates = [
+                        (i.reporting_date).strftime(clusteringDefault) for i in incidents 
+                        if (date <= i.reporting_date) and (i.reporting_date <=terminoCopaConf and i['operations_center']==cop)
+                    ]
+    
+    return incidentsDates
+
 def get_all_incidentes_from_file(inicioCopaConf, terminoCopaConf):
 
     datesFromFile = read_file('clusterizado.c2')
@@ -277,25 +289,59 @@ def get_COP_incidents(inicioCopaConf, terminoCopaConf):
             if (inicioCopaConf <= incidente.reporting_date) and (incidente.reporting_date <=terminoCopaConf)
             ]
     
-    return collections.Counter(cops)    
-    
-def get_statics(dataArray):
+    return collections.Counter(cops)
 
-    print (dataArray)
-    sizeData, (minimum,maximum),arithmeticMean,variance,skeness,kurtosis = stats.describe(map(float,dataArray))
-    #mean = np.mean(np.array(map(float,dataArray)))
-    mean = np.mean(map(float,dataArray))
-    vara = np.var(map(float,dataArray))
+def get_qtd_actions_near_date(cop, date):
 
-    print arithmeticMean,variance
-    print mean, vara
-
-    print "Correlacao: ",stats.pearsonr(map(float,dataArray),map(float,dataArray))
-
-if __name__ == "__main__":
-    
+    clusteringDefault = '%Y/%m/%d'
     inicioCopaConf = datetime(2013,6,10)
     terminoCopaConf = datetime(2013,7,3)
+    #clusterPorHora = '%Y/%B/%d %H:%m:%S'
+    sincronizacoes = Sincronizacao.get_all()
+    tmp = [sinc for sinc in sincronizacoes if sinc.cop_responsavel['id']==cop]
+    qtde = 0
+    print len(tmp)
+    for sinc in sincronizacoes:
+        actionsDates = []
+        if (sinc.cop_responsavel['id']== cop or cop == 'TODOS'):
+         
+            for action in sinc.acoes:
+                print action.inicio, date - action.inicio
+                if (
+                   #((action.tipo == 'INTERVALO') and (action.inicio <= date) and (date <=action.fim)) or
+                   #((action.tipo == 'PONTUAL')and (action.inicio <= date))
+                     action.tipo == 'INTERVALO' and 
+                     action.inicio >=inicioCopaConf and
+                     action.fim <= terminoCopaConf and 
+                     (date - action.inicio).days <= 1 and 
+                     (action.fim - date).days <=1                 
+                   #or
+                   #((action.tipo == 'PONTUAL') and (date - action.inicio).days <= 1)
+                ):
+                    
+                    qtde = qtde + len(actionsDates)
+
+    return qtde
+
+def get_statics(dataArray):
+
+    sizeData, (minimum,maximum),arithmeticMean,variance,skeness,kurtosis = stats.describe(map(float,dataArray))
+    mean = np.mean(map(float,dataArray))
+    vara = np.var(map(float,dataArray))
+#    print "Correlacao: ",stats.pearsonr(map(float,dataArray),map(float,dataArray))
+
+def incidentes_actions_total():
+
+    """
+        realiza o estudo de todos os incidentes e açoes planejadas perto da data do incidente
+        imprime a correlação
+        todas as sedes e o total
+    """    
+    inicioCopaConf = datetime(2013,6,10)
+    terminoCopaConf = datetime(2013,7,3)
+
+    matchDays = [datetime(2013,6,15),datetime(2013,6,16),datetime(2013,6,17),datetime(2013,6,19),datetime(2013,6,20),
+                 datetime(2013,6,22),datetime(2013,6,23),datetime(2013,6,26),datetime(2013,6,27),datetime(2013,6,30)]
 
 
     # para recuperar do banco
@@ -315,22 +361,28 @@ if __name__ == "__main__":
 
     axisX = []
     axisY = []
+    actionsPerDateTotal = []
     for key in sortedIncidentDates:
         axisX.append(datetime.strptime(key,"%Y/%m/%d"))
         axisY.append(dictionaryIncidentsDates[key])
+        actionsPerDateTotal.append(get_qtd_actions_near_date('TODOS',datetime.strptime(key,"%Y/%m/%d")))
     
-    get_statics(axisY)
+    print "Correlacao: ",stats.pearsonr(map(float,axisY),map(float,actionsPerDateTotal))
+    #plot_graph(axisX,axisY,actionsPerDateTotal)
+    #get_statics(axisY)
     #plot_hist(axisX,axisY)
 
-    #cops = get_COP_incidents(inicioCopaConf,terminoCopaConf)
+    cops = get_COP_incidents(inicioCopaConf,terminoCopaConf)
 
     axisYbyCops = {}
     axisXbyCops = {}
+    actionsPerDateByCops = {}
 
-    """
+    
     for cop in cops.keys():
         axisYbyCops[cop]=[]
         axisXbyCops[cop]=[]
+        actionsPerDateByCops[cop] = []
         incidentsDates =  get_incidents_by_cop_from_DB(inicioCopaConf,terminoCopaConf,cop)
         dictionaryIncidentsDates = {}
         #Faz a contagem de incidentes por data segundo o padrão de cluster
@@ -340,21 +392,48 @@ if __name__ == "__main__":
         sortedIncidentDates = sorted(set(dictionaryIncidentsDates)) 
         for key in sortedIncidentDates:
             axisXbyCops[cop].append(datetime.strptime(key,"%Y/%m/%d"))
-            axisYbyCops[cop].append(dictionaryIncidentsDates[key])                  
+            axisYbyCops[cop].append(dictionaryIncidentsDates[key])
+            actionsPerDateByCops[cop].append(get_qtd_actions_near_date(cop,datetime.strptime(key,"%Y/%m/%d")))
+        print cop, " - Correlacao: ",stats.pearsonr(map(float,axisYbyCops[cop]),map(float,actionsPerDateByCops[cop]))
 
-    plot_total('line',
-                axisX,axisY,
-                axisXbyCops['COC'],axisYbyCops['COC'],
-                axisXbyCops['CC2 - FTC - SSA'],axisYbyCops['CC2 - FTC - SSA'],
-                axisXbyCops['CCDA - FOR'],axisYbyCops['CCDA - FOR'],
-                axisXbyCops['CCDA - REC'],axisYbyCops['CCDA - REC'],
-                axisXbyCops['CCDA - SSA'],axisYbyCops['CCDA - SSA'],
-                axisXbyCops['CCDA - RIO'],axisYbyCops['CCDA - RIO'],
-                axisXbyCops['CCDA - BSB'],axisYbyCops['CCDA - BSB'],
-                axisXbyCops['CCDA - BHZ'],axisYbyCops['CCDA - BHZ']                
-                )
+    plot_total('line',   
+                axisX,axisY,actionsPerDateTotal,
+                axisXbyCops['COC'],axisYbyCops['COC'],actionsPerDateByCops['COC'],
+                axisXbyCops['CC2 - FTC - SSA'],axisYbyCops['CC2 - FTC - SSA'],actionsPerDateByCops['CC2 - FTC - SSA'],
+                axisXbyCops['CCDA - FOR'],axisYbyCops['CCDA - FOR'],actionsPerDateByCops['CCDA - FOR'],
+                axisXbyCops['CCDA - REC'],axisYbyCops['CCDA - REC'],actionsPerDateByCops['CCDA - REC'],
+                axisXbyCops['CCDA - SSA'],axisYbyCops['CCDA - SSA'],actionsPerDateByCops['CCDA - SSA'],
+                axisXbyCops['CCDA - RIO'],axisYbyCops['CCDA - RIO'],actionsPerDateByCops['CCDA - RIO'],
+                axisXbyCops['CCDA - BSB'],axisYbyCops['CCDA - BSB'],actionsPerDateByCops['CCDA - BSB'],
+                axisXbyCops['CCDA - BHZ'],axisYbyCops['CCDA - BHZ'],actionsPerDateByCops['CCDA - BHZ']
+               )
+                   
+    #actionsDates =  get_all_actions_from_DB(inicioCopaConf,terminoCopaConf)
+    #print actionsDates[0]
+    #print (collections.Counter(actionsDates))
+
+    #print get_qtd_actions_near_date('CCDA - FOR',datetime(2013,06,17))
+    
+def incidents_near_matchDays():
+
     """
-    
-    actionsDates =  get_all_actions_from_DB(inicioCopaConf,terminoCopaConf)
-    print collections.Counter(actionsDates)
-    
+        Faz a estatistica tendo como base os dias dos jogos
+        Para cada dia de jogo calcula o número de incidentes e açoes próximas
+    """
+    inicioCopaConf = datetime(2013,6,10)
+    terminoCopaConf = datetime(2013,7,3)
+
+    matchDays = [datetime(2013,6,15),datetime(2013,6,16),datetime(2013,6,17),datetime(2013,6,19),datetime(2013,6,20),
+                 datetime(2013,6,22),datetime(2013,6,23),datetime(2013,6,26),datetime(2013,6,27),datetime(2013,6,30)]
+
+
+if __name__ == "__main__":
+
+    #incidentes_actions_total()
+    inicioCopaConf = datetime(2013,6,10)
+    terminoCopaConf = datetime(2013,7,3)
+    t = inicioCopaConf - terminoCopaConf
+    print t.days
+    print get_qtd_actions_near_date('CCDA - RIO',datetime(2013,06,30))
+
+   
